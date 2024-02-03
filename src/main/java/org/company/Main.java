194969551592f316487256d9f85cdb82aa44ea00
JCRodriguez93 <jorge_madro@hotmail.com>
2024-02-03 20:18:53 +0100

    fixing an update error

--- src/main/java/org/company/Main.java
+++ src/main/java/org/company/Main.java
@@ -1,5 +1,21 @@
-package org.company;public class Main {
+package org.company;
+
+import java.sql.Connection;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        DBPostgreFunctions dbFunctions = new DBPostgreFunctions();
+        // Intentar establecer la conexión
+        Connection conn = dbFunctions.connectToDB("test", "userdb", "passdb");
+        // Verificar si la conexión se ha realizado con éxito
+        if (conn != null) {
+            System.out.println("Connection established");
+        // Hacer otras cosas si fuese necesario
+        } else {
+            System.out.println("Connection failed. Unable to proceed.");
+        }
+
+
     }
 }
